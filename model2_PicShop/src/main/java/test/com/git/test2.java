cf2cc2c70b96236ca98d5e2ddae5b6c4896260bf
Minhyeok1998 <tom2945000@gmail.com>
2022-04-26 11:32:51 +0900

    com.git.test 삭제 완료

--- model2_PicShop/src/main/java/test/com/git/test2.java
+++
@@ -1,7 +0,0 @@
-package test.com.git;
-
-public class test2 {
-	public static void main(String[] args) {
-		System.out.println("테스트 중!");
-	}
-}
--- model2_PicShop/src/main/java/test/com/git/test3.java
+++
@@ -1,7 +0,0 @@
-package test.com.git;
-
-public class test3 {
-	public static void main(String[] args) {
-		System.out.println("테스트 중!");
-	}
-}
