bf1d39c8ef11b0fab76bdf7b19f42c02158b20e5
Minhyeok1998 <tom2945000@gmail.com>
2022-04-26 14:32:18 +0900

    민혁- 관리자/서비스 상단 nav 1차 완성

--- model2_PicShop/src/main/java/pic_shop/com/dao/MemberDaoAble.java
+++ model2_PicShop/src/main/java/pic_shop/com/dao/MemberDaoAble.java
@@ -1,5 +1,15 @@
 package pic_shop.com.dao;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import pic_shop.com.vo.MemberVo;
+
 public interface MemberDaoAble {
 
+	public List<MemberVo> list(int page) throws ClassNotFoundException, SQLException ; 
+	public MemberVo detail(String id) throws ClassNotFoundException, SQLException ; 
+	public boolean insert(MemberVo mem) throws ClassNotFoundException, SQLException ; 
+	public boolean update(MemberVo mem) throws ClassNotFoundException, SQLException ; 
+	public boolean delete(String id) throws ClassNotFoundException, SQLException ; 
 }
--- model2_PicShop/src/main/webapp/index.jsp
+++ model2_PicShop/src/main/webapp/index.jsp
@@ -7,14 +7,11 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<%@ include file="nav_header.jsp" %>
-
+	<%@ include file="admin_header_nav.jsp" %>
+<!-- 
 	<h1>Main 페이지</h1>
-	<h2>하이하이</h2>
+	<h2>하이하이</h2> -->
 
-	<h1>Main 병합 테스트</h1>
-	
-	<h2>병합 테스트입니다.</h2>
 
 </body>
 </html>
